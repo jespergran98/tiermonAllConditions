effc5cb76a612cac10cb7e827a021945b4296db8
jespergran98 <jesper25sfj@gmail.com>
2025-10-19 22:55:00 +0200

    working S+ rating

--- renderDecks.js
+++ renderDecks.js
@@ -133,7 +133,7 @@ function createDeckCard(deck) {
       <div class="deck-rank">#${deck.rank}</div>
     </div>
     
-    <div class="tier-badge tier-${escapeHtml(deck.tier.replace('+', '\\+'))}">${escapeHtml(deck.tier)}</div>
+    <div class="tier-badge tier-${escapeHtml(deck.tier)}">${escapeHtml(deck.tierDisplay)}</div>
     
     <div class="deck-stats">
       <div class="stat-item">
